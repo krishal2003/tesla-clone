b87c115be65ed826303702424cc22e48f3b8782e
Krishal Basnet <krishalbasnet2003@gmail.com>
2023-02-04 17:29:31 +0545

    first

---
+++ src/components/Section.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        
+        </ButtonGroup>
+
+        <DownArrow src="/images/down-arrow.svg"></DownArrow>
+      </Buttons>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background: #3b3d40;
+  height: 40px;
+  width: 250px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background: white;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  //   margin-top: 20px;
+  height: 40px;
+  overflow-x: hidden;
+  cursor: pointer;
+  animation: animateDown infinite 1.4s;
+`;
+
+const Buttons = styled.div``;
